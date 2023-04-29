95c81fbe69046e91312e41124efe889d071d06ee
CrispyCreme10 <jordanrenner69@gmail.com>
2023-04-28 22:09:22 -0500

    baller status

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,36 @@ struct Solution {
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let first = 0;
-        let second = 1;
+        let mut gap: usize;
+        let mut first_index = 0;
+        let mut second_index = 1;
+
+        println!("TEST");
+
+        loop {
+
+            if nums[first_index] + nums[second_index] == target {
+                return vec![first_index.try_into().unwrap(), second_index.try_into().unwrap()]
+            }
+
+            // reset & increase gap
+            if second_index == nums.len() - 1 {
+                // break condition
+                if first_index == 0 {
+                    break;
+                }
+
+                gap = 2;
+                first_index = 0;
+                second_index = first_index + gap;
+                continue;
+            }
+
+            // increment indices
+            first_index += 1;
+            second_index += 1;
+        }
+
+        vec![]
     }
 }
